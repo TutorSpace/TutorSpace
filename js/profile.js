85ed5ee432440c0557b976cc4102e57fdbad18fa
luoshuaiqing <shuaiqin@usc.edu>
2020-03-02 14:31:29 -0800

    updated

---
+++ js/profile.js
@@ -0,0 +1,11 @@
+let showed_id = 0;
+$('.profile-nav > .nav-link').click(function() {
+    
+    $('.profile-nav > .nav-link').each(function() {
+        $(this).removeClass('active');
+    })
+        
+    $(this).toggleClass('active');
+
+    if($this.)
+});
