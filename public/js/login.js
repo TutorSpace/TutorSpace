25522168dcd3853ec22fd891f7144e1dcbd8ad65
Shuaiqing Luo <shuaiqin@usc.edu>
2020-06-25 18:47:04 -0700

    update

--- public/js/login.js
+++ public/js/login.js
@@ -81,30 +81,30 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./resources/js/auth/register.js":
-/*!***************************************!*\
-  !*** ./resources/js/auth/register.js ***!
-  \***************************************/
+/***/ "./resources/js/auth/login.js":
+/*!************************************!*\
+  !*** ./resources/js/auth/login.js ***!
+  \************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/Users/luoshuaiqing/Desktop/TutorSpace/resources/js/auth/register.js'");
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/Users/luoshuaiqing/Desktop/TutorSpace/resources/js/auth/login.js'");
 
 /***/ }),
 
-/***/ 1:
-/*!*********************************************!*\
-  !*** multi ./resources/js/auth/register.js ***!
-  \*********************************************/
+/***/ 2:
+/*!******************************************!*\
+  !*** multi ./resources/js/auth/login.js ***!
+  \******************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/luoshuaiqing/Desktop/TutorSpace/resources/js/auth/register.js */"./resources/js/auth/register.js");
+module.exports = __webpack_require__(/*! /Users/luoshuaiqing/Desktop/TutorSpace/resources/js/auth/login.js */"./resources/js/auth/login.js");
 
 
 /***/ })
