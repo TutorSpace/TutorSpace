98f6f4bde9eeb61d0d9344f241ce60cc5da3ac19
Shuaiqing Luo <shuaiqin@usc.edu>
2020-07-02 15:15:39 -0700

    update nav bar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,14 +13,7 @@ index
 
 @section('content')
 
-<header>
-    <nav class="nav p-fixed w-100 d-flex align-items-center">
-        <a class="fc-blue-primary brand-name" href="{{ route('index') }}">TutorSpace</a>
-        <a class="nav__item" href="#">Forum</a>
-        <a class="nav__item" href="#">Support</a>
-        <a class="btn btn-lg nav__item ml-auto btn-outline-student" href="#index__bottom" id="btn-sign-in">Sign In</a>
-    </nav>
-</header>
+@include('partials.nav')
 
 <section class="index__top p-relative">
     <div class="absCenter d-flex flex-column align-items-center">
@@ -217,7 +210,7 @@ index
         }, 500);
     });
 
-    $('#btn-sign-in').click(function() {
+    $('#nav-btn-sign-in').click(function() {
         $('html, body').animate({
             scrollTop: $('#index__bottom').offset().top - $('.nav').height()
         }, 1000);
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,8 +1,35 @@
 <header>
-    <nav class="nav p-fixed w-100 d-flex align-items-center">
-        <a class="fc-blue-primary brand-name" href="{{ route('index') }}">TutorSpace</a>
-        <a class="nav__item" href="#">Forum</a>
-        <a class="nav__item" href="#">Support</a>
-        <a class="btn btn-lg nav__item ml-auto btn-outline-student" href="#" >Sign In</a>
+    <nav class="nav p-fixed w-100 d-flex align-items-center
+        @auth
+            @if (Auth::user()->is_tutor)
+                nav-auth nav-auth--tutor
+            @else
+                nav-auth nav-auth--student
+            @endif
+        @else
+            nav-guest
+        @endauth
+    ">
+
+
+        <a class="brand-name" href="@auth{{ route('index') }} @else {{ route('home') }}@endauth">TutorSpace</a>
+
+        <div class="nav-left d-flex align-items-center">
+            <a class="nav__item" href="#">Forum</a>
+            <a class="nav__item" href="#">Support</a>
+            <form action="" method="GET" class="nav__form p-relative">
+                <input type="text" class="nav__form__search form-control form-control-lg">
+                <svg class="nav__form__svg">
+                    <use xlink:href="{{asset('assets/sprite.svg#icon-user')}}"></use>
+                </svg>
+            </form>
+        </div>
+
+
+        <div class="nav-right">
+            @guest
+                <a class="nav__item btn btn-lg ml-auto btn-outline-student" href="#" id="nav-btn-sign-in">Sign In</a>
+            @endguest
+        </div>
     </nav>
 </header>
