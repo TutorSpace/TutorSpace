ded29e78933bab4ad88b305fccaf3907d2341d75
Shuaiqing Luo <shuaiqin@usc.edu>
2020-07-03 01:14:30 -0700

    update nav bar

--- public/js/app.js
+++ public/js/app.js
@@ -43962,11 +43962,23 @@ $(document).ready(function () {
   $(window).resize(function () {
     adjustSquareSize();
   }); // for nav animation
-  // setTimeout(function() {
-  //     $('.message-welcome').hide();
-  //     $('.message-welcome ~ *').addClass('nav-fade-in-animation');
-  //     $('.message-welcome ~ *').show();
-  // }, 3700);
+
+  setTimeout(function () {
+    $('.message-welcome').hide();
+    $('.nav-right__svg-container').addClass('nav-fade-in-animation');
+    $('.nav-right__profile-img').addClass('nav-fade-in-animation');
+    $('.nav-right__svg-container').show();
+    $('.nav-right__profile-img').show();
+  }, 3700);
+  $('.nav-right__profile-img').click(function () {
+    if ($('.nav-right .nav-toggle-sm').is(":hidden")) {
+      $('.profile-img-dropdown').toggle();
+    }
+  });
+  $('.svg-list').click(function () {
+    $('.profile-img-dropdown').hide();
+    $(this).next().toggle();
+  });
 });
 
 /***/ }),
