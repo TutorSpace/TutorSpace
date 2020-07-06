b363dca2fbf2ec19ecf46bfe9982a7e565eb14ef
Shuaiqing Luo <shuaiqin@usc.edu>
2020-07-06 16:47:46 -0700

    update

--- public/js/forum/create.js
+++ public/js/forum/create.js
@@ -100,10 +100,14 @@ $('.btn-post-type').click(function () {
 tinymce.init({
   selector: 'textarea',
   // change this value according to your HTML
-  plugins: ['advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker', 'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking', 'table emoticons template paste help imagetools'],
+  plugins: ['advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker', 'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime nonbreaking', 'table emoticons template paste help imagetools'],
   height: 300,
   a_plugin_option: true,
-  a_configuration_option: 400
+  a_configuration_option: 400,
+  images_upload_url: '/upload.php',
+  images_upload_handler: function images_upload_handler(info, success, failure) {
+    alert(info);
+  }
 });
 $('#tags').select2({
   placeholder: "Add post tags here..."
