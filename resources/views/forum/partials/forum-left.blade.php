d60ccd6cfd0c9f0d629b7f820acd799aa28c7fe7
Shuaiqing Luo <shuaiqin@usc.edu>
2020-07-06 00:48:13 -0700

    update

--- resources/views/forum/partials/forum-left.blade.php
+++ resources/views/forum/partials/forum-left.blade.php
@@ -9,24 +9,30 @@
             <path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
         </svg>
     </div>
-    <ul class="forum-left__list">
-        <li class="forum-left__list-item">
-            <svg class="forum-left__list-svg">
-                <use xlink:href="{{asset('assets/sprite.svg#icon-star')}}"></use>
-            </svg>
-            <a class="forum-left__list-content" href="#">Popular Posts</a>
+    <ul class="forum-left-sm__list">
+        <li class="forum-left-sm__list-item">
+            <a class="forum-left-sm__list-content" href="#">
+                <svg class="forum-left-sm__list-svg">
+                    <use xlink:href="{{asset('assets/sprite.svg#icon-star')}}"></use>
+                </svg>
+                <span>Popular Posts</span>
+            </a>
         </li>
-        <li class="forum-left__list-item">
-            <svg class="forum-left__list-svg">
-                <use xlink:href="{{asset('assets/sprite.svg#icon-star')}}"></use>
-            </svg>
-            <a class="forum-left__list-content" href="#">My Posts</a>
+        <li class="forum-left-sm__list-item">
+            <a class="forum-left-sm__list-content" href="#">
+                <svg class="forum-left-sm__list-svg">
+                    <use xlink:href="{{asset('assets/sprite.svg#icon-star')}}"></use>
+                </svg>
+                <span>My Posts</span>
+            </a>
         </li>
-        <li class="forum-left__list-item">
-            <svg class="forum-left__list-svg">
-                <use xlink:href="{{asset('assets/sprite.svg#icon-star')}}"></use>
-            </svg>
-            <a class="forum-left__list-content" href="#">My Follows</a>
+        <li class="forum-left-sm__list-item">
+            <a class="forum-left-sm__list-content" href="#">
+                <svg class="forum-left-sm__list-svg">
+                    <use xlink:href="{{asset('assets/sprite.svg#icon-star')}}"></use>
+                </svg>
+                <span>My Follows</span>
+            </a>
         </li>
     </ul>
 </div>
