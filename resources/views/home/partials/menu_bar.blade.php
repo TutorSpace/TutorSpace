2d8773460ae52a8fe4db34bc0bf4195235d3ba71
Shuaiqing Luo <shuaiqin@usc.edu>
2020-08-11 23:02:45 -0700

    update

--- resources/views/home/partials/menu_bar.blade.php
+++ resources/views/home/partials/menu_bar.blade.php
@@ -1,6 +1,11 @@
 <nav class="menu-bar">
     <ul class="menu-bar__lists">
-        <li>Dashboard</li>
+        <li data-href="#">
+            <span>Dashboard</span>
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+            </svg>
+        </li>
         <li data-href="#">
             <span>Tutor Sessoins</span>
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
