6f7934e2bb98436c51ffc6ae43d724ca43ba4003
Shuaiqing Luo <shuaiqin@usc.edu>
2020-08-14 13:24:05 -0700

    update

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -33,12 +33,6 @@ bg-student
     @include('home.partials.menu_bar')
     <main class="home__content">
         <div class="container home__header-container">
-            <div class="heading-container">
-                <p class="heading">Dashboard</p>
-                <span>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed enim blanditiis ipsam nesciunt quia culpa eaque eligendi
-                </span>
-            </div>
             @include('home.partials.header')
         </div>
 
