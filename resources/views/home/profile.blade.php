413d580138f7b4464e4c31ac4d6cfa3944fa26a7
Shuaiqing Luo <shuaiqin@usc.edu>
2020-08-16 02:02:29 -0700

    update

--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -36,7 +36,59 @@ bg-student
             </div>
         </div>
 
+        <form class="container profile">
+            <div class="row">
+                <h5 class="w-100 profile__heading">Personal Information</h5>
+                <div class="profile__form-row">
+                    <div>
+                        <label for="" class="profile__label">First Name</label>
+                        <input type="text" class="profile__input form-control form-control-lg" placeholder="Shuaiqing" readonly>
+                    </div>
+                    <div>
+                        <label for="" class="profile__label">Last Name</label>
+                        <input type="text" class="profile__input form-control form-control-lg" placeholder="Luo" disabled readonly>
+                    </div>
+                </div>
 
+                <div class="profile__form-row mt-3">
+                    <div>
+                        <label for="" class="profile__label">First Major</label>
+                        <input type="text" class="profile__input form-control form-control-lg" value="Computer Science">
+                    </div>
+                    <div>
+                        <label for="" class="profile__label">Second Major (optional)</label>
+                        <input type="text" class="profile__input form-control form-control-lg" value="">
+                    </div>
+                    <div>
+                        <label for="" class="profile__label">Minor (optional)</label>
+                        <input type="text" class="profile__input form-control form-control-lg" value="">
+                    </div>
+                </div>
+
+                <div class="profile__form-row mt-3">
+                    <div>
+                        <label for="" class="profile__label">Class Standing</label>
+                        <input type="text" class="profile__input form-control form-control-lg" value="Junior">
+                    </div>
+                    <div class="gpa">
+                        <label for="" class="profile__label">GPA</label>
+                        <input type="text" class="profile__input form-control form-control-lg" value="4.0">
+                    </div>
+                </div>
+
+                <h5 class="w-100 profile__heading">Tutor Information</h5>
+                <div class="profile__form-row">
+                    <div>
+                        <label for="" class="profile__label">Courses you would like to tutor</label>
+                        <input type="text" class="profile__input form-control form-control-lg" value="">
+                    </div>
+                    <div class="hourly-rate">
+                        <label for="" class="profile__label">Hourly Rate</label>
+                        <input type="text" class="profile__input form-control form-control-lg" value="18">
+                    </div>
+                </div>
+            </div>
+        </form>
 
 
     </main>
