ca88d6d99f6f8768417f3e49e395b1ab82f33f03
Shuaiqing Luo <shuaiqin@usc.edu>
2020-08-29 17:52:04 -0700

    update

--- resources/views/home/tutor-sessions.blade.php
+++ resources/views/home/tutor-sessions.blade.php
@@ -32,12 +32,13 @@ bg-student
     @include('home.partials.menu_bar')
     <main class="home__content">
         <div class="container col-layout-2 home__header-container">
-            <div class="heading-container">
+            <div class="heading-container mb-5">
                 <p class="heading">Tutor Sessions</p>
                 <span>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed enim blanditiis ipsam nesciunt quia culpa eaque eligendi
                 </span>
             </div>
+            @include('home.partials.header')
         </div>
 
         <div class="container col-layout-2">
