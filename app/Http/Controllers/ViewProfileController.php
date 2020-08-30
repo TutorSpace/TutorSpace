5f197d781c2449bef526d6e18eebcd6f2ca587b2
Shuaiqing Luo <shuaiqin@usc.edu>
2020-08-29 19:06:22 -0700

    start updating view profile page

--- app/Http/Controllers/ViewProfileController.php
+++ app/Http/Controllers/ViewProfileController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class ViewProfileController extends Controller
 {
-    //
+    public function index(User $user) {
+        return view('home.view_profile.index', compact('user'));
+    }
 }
--- resources/views/home/view_profile/index.blade.php
+++ resources/views/home/view_profile/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Dashboard')
+@section('title', "$user->first_name's Profile")
 
 @section('body-class')
 bg-white-dark-4
@@ -17,29 +17,20 @@ bg-student
 {{-- fullcalendar --}}
 <link href='https://use.fontawesome.com/releases/v5.0.6/css/all.css' rel='stylesheet'>
 <script src='{{asset('fullcalendar/main.min.js')}}'></script>
-
-{{-- plotly --}}
-<script src="{{ asset('js/plotly.js') }}"></script>
 @endsection
 
 @section('content')
 
 @include('partials.nav')
 
-@if(Auth::user()->is_tutor)
-    @include('home.partials.availableTimeConfirmationModal')
-    @include('home.partials.availableTimeDeleteConfirmationModal')
-@endif
-
-<div class="container-fluid home p-relative">
-    @include('home.partials.menu_bar')
-    <main class="home__content">
-        <div class="container col-layout-3 home__header-container">
+<div class="container-fluid view p-relative">
+    <main class="view__content">
+        <div class="container home__header-container">
             @include('home.partials.header')
         </div>
 
         @if (Auth::user()->is_tutor)
-        <div class="container col-layout-3">
+        <div class="container">
             <div class="row">
                 <h5 class="mb-2 w-100">You Have 3 New Tutor Requests!</h5>
                 <div class="info-boxes info-boxes--sm-card">
@@ -62,7 +53,7 @@ bg-student
             </div>
         </div>
 
-        <div class="container col-layout-3 col-layout-3--hidden">
+        <div class="container">
             <div class="row">
                 <h5 class="mb-2 w-100">New Notifications</h5>
                 <div class="info-boxes">
@@ -78,7 +69,7 @@ bg-student
             </div>
         </div>
 
-        <div class="container col-layout-3">
+        <div class="container">
             <div class="row home__row-columns-2">
                 <div class="pr-0" id="calendar-container">
                     <h5 class="w-100 calendar-heading">Calendar</h5>
@@ -90,7 +81,7 @@ bg-student
                         <span class="note">Note: All time in the calender are based on PST.</span>
                     </div>
                 </div>
-                <div class="info-cards col-layout-3--hidden" id="upcoming-sessions-container">
+                <div class="info-cards" id="upcoming-sessions-container">
                     <div class="d-flex align-items-center justify-content-between mb-1 flex-100">
                         <h5 class="mb-0 ws-no-wrap">Upcoming Sessions</h5>
                         <button class="btn btn-link fs-1-2 fc-grey btn-view-all-info-cards ws-no-wrap">View All</button>
@@ -128,7 +119,7 @@ bg-student
             </div>
         </div> --}}
 
-        <div class="container col-layout-3">
+        <div class="container">
             <div class="row">
                 <h5 class="mb-2 w-100">Data Visualization</h5>
                 <div class="home__data-visualizations">
@@ -143,7 +134,7 @@ bg-student
         </div>
 
         @else
-        <div class="container col-layout-3">
+        <div class="container">
             <div class="row">
                 <h5 class="mb-2 w-100">Congrats! Your Tutor Request has been approved!</h5>
                 <div class="info-boxes">
@@ -170,7 +161,7 @@ bg-student
             </div>
         </div>
 
-        <div class="container col-layout-3">
+        <div class="container">
             <div class="row">
                 <div class="d-flex justify-content-between align-items-center w-100 mb-2">
                     <h5>Upcoming Sessions</h5>
@@ -190,7 +181,7 @@ bg-student
             </div>
         </div>
 
-        <div class="container col-layout-3">
+        <div class="container">
             <div class="row">
                 <h5 class="mb-2 w-100">Bookmarked Tutors</h5>
 
@@ -208,7 +199,7 @@ bg-student
         </div>
 
         <div class="container-fluid recommended-tutors-bg-container">
-            <div class="container col-layout-3">
+            <div class="container">
                 <div class="row">
                     <div class="mb-2 w-100 d-flex justify-content-between align-center">
                         <h5>Tutors You May Want to Know</h5>
@@ -221,7 +212,7 @@ bg-student
             </div>
         </div>
 
-        <div class="container col-layout-3">
+        <div class="container">
             <div class="row">
                 <h5 class="mb-2 w-100">Tutor Requests</h5>
                 <div class="info-boxes tutor-requests">
@@ -282,70 +273,7 @@ bg-student
 
         @endif
 
-        <div class="container col-layout-3">
-            <div class="row forum mt-0">
-                <h5 class="w-100">Recommended Posts</h5>
-                <div class="col-12 col-md-9 post-previews px-0">
-                    @include('forum.partials.post-preview-general')
-                </div>
-                <div class="col-12 col-md-3 forum-data-container">
-                    <div class="forum-data">
-                        {{-- <svg class="notification-indicator" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="7.5" cy="7.5" r="7.5" fill="#FFBC00"/>
-                        </svg> --}}
-                        <span class="title">My Posts</span>
-                        <a class="number" href="{{ route('posts.my-posts') }}">{{ Auth::user()->posts()->count() }}</a>
-                    </div>
-                    <div class="forum-data">
-                        <span class="title">Participated</span>
-                        <a class="number" href="{{ route('posts.my-participated') }}">{{ Auth::user()->postsReplied()->count() }}</a>
-                    </div>
-                    <div class="forum-data">
-                        <span class="title">Followed</span>
-                        <a class="number" href="{{ route('posts.my-follows') }}">{{ Auth::user()->followedPosts()->count() }}</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
     </main>
-
-    <section class="home__side-bar">
-        <div class="home__board">
-        </div>
-        <div class="home__side-bar__notifications">
-            <h5 class="mb-2 w-100">New Notifications</h5>
-            <div class="info-boxes">
-                @include('home.partials.notification', [
-                    'isCancellationNotification' => true,
-                    'notificationContent' => 'Nemo Enim'
-                ])
-                @include('home.partials.notification', [
-                    'isBestReplyNotification' => true,
-                    'notificationContent' => 'Testing Post 1'
-                ])
-
-            </div>
-        </div>
-
-        <div class="home__side-bar__upcoming-sessions">
-            <div class="info-cards">
-                <div class="d-flex align-items-center justify-content-between mb-1 flex-100">
-                    <h5 class="mb-0 ws-no-wrap">Upcoming Sessions</h5>
-                    <button class="btn btn-link fs-1-2 fc-grey btn-view-all-info-cards ws-no-wrap">View All</button>
-                </div>
-                @include('home.partials.upcoming_session_card')
-                @include('home.partials.upcoming_session_card')
-                @include('home.partials.upcoming_session_card')
-                @include('home.partials.upcoming_session_card', [
-                    'hidden' => true
-                ])
-                @include('home.partials.upcoming_session_card', [
-                    'hidden' => true
-                ])
-            </div>
-        </div>
-    </section>
 </div>
 
 
@@ -353,32 +281,8 @@ bg-student
 
 @section('js')
 
-@if(Auth::user()->is_tutor)
-    @include('home.partials.calendar-tutor')
-@endif
 
-<script>
-let storageUrl = "{{ Storage::url('') }}";
-@if(!Auth::user()->is_tutor)
-    function getRecommendedTutors() {
-        $.ajax({
-            type:'GET',
-            url: '{{ route('recommended-tutors') }}?refresh=true',
-            success: (data) => {
-                $('.recommended-tutors').html(data);
-                console.log(data);
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        });
-    }
-    // refresh recommended tutors
-    $('#btn-refresh').click(function() {
-        getRecommendedTutors();
-    });
-@endif
-</script>
+
 
 {{-- for data visualization --}}
 @include('home.partials.data-visualization')
