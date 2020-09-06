2a69d0f1f39f9413a1c99c336aa8714a2ec5009b
Shuaiqing Luo <shuaiqin@usc.edu>
2020-09-05 20:24:14 -0700

    update

---
+++ resources/views/home/partials/notification--sidebar.blade.php
@@ -0,0 +1,29 @@
+@if (isset($isCancellationNotification) && $isCancellationNotification)
+<div class="notification">
+    <div class="notification--left">
+        <img src="{{ Storage::url(Auth::user()->profile_pic_url) }}" alt="profile img">
+    </div>
+    <div class="notification--content">
+        <span>Session Cancelled</span>
+        <span class="subtitle">Computer Science &middot; 08/21/20</span>
+    </div>
+
+    <div class="notification--right">
+        <span>15 hours ago</span>
+    </div>
+</div>
+@elseif(isset($isBestReplyNotification) && $isBestReplyNotification)
+<div class="notification">
+    <div class="notification--left">
+        <div class="notification--best-reply"></div>
+    </div>
+    <div class="notification--content">
+        <span>Marked As Best Reply</span>
+        <span class="subtitle">{{ $notificationContent }}</span>
+    </div>
+
+    <div class="notification--right">
+        <span>15 hours ago</span>
+    </div>
+</div>
+@endif
