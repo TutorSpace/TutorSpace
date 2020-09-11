14a6a593472abe52d0e21698a03e6216d9901845
Shuaiqing Luo <shuaiqin@usc.edu>
2020-09-10 21:54:52 -0700

    update

--- resources/views/chatting/side-bar-chatting-msg.blade.php
+++ resources/views/chatting/side-bar-chatting-msg.blade.php
@@ -1,1 +1,18 @@
-<li class="chatting-msg">chatting message 1</li>
+<li class="chatting-msg">
+    <div class="chatting-box">
+        <div class="img-container">
+            <img src="{{ Storage::url(Auth::user()->profile_pic_url) }}" alt="user img">
+        </div>
+        <div class="content-container">
+            <span class="user-name">
+                Bessie Cooper
+            </span>
+            <span class="message-content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima beatae veritatis aperiam laudantium. Voluptatibus doloremque ad ipsa, asperiores necessitatibus totam quaerat quia aliquam, adipisci, mollitia cum nemo enim? Accusamus, aut?
+            </span>
+        </div>
+        <div class="time-container">
+            dgsd
+        </div>
+    </div>
+</li>
