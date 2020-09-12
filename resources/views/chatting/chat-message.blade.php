8d9b0a9aa5353398ece1947e3154f07303e93137
Shuaiqing Luo <shuaiqin@usc.edu>
2020-09-12 15:44:09 -0700

    update

--- resources/views/chatting/chat-message.blade.php
+++ resources/views/chatting/chat-message.blade.php
@@ -1,25 +1,25 @@
 @if ($myMessage)
 <div class="message message--self">
-    <div class="img-container">
-        <img src="{{ Storage::url(Auth::user()->profile_pic_url) }}" alt="user img">
+    <div class="time-container">
+        {{ $time }}
     </div>
     <div class="message-content-container">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum atque doloremque autem modi dolores nihil nisi eos dolore voluptatibus cum, in explicabo, cumque non sit earum ratione culpa praesentium ea!
-    </div>
-    <div class="time-container">
-        5:38pm
+        {{ $content }}
     </div>
 </div>
 @else
-<div class="message message-other">
+<div class="message message--other">
     <div class="img-container">
         <img src="{{ Storage::url(Auth::user()->profile_pic_url) }}" alt="user img">
     </div>
     <div class="message-content-container">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum atque doloremque autem modi dolores nihil nisi eos dolore voluptatibus cum, in explicabo, cumque non sit earum ratione culpa praesentium ea!
+        <span class="content">
+            {{ $content }}
+        </span>
+
     </div>
     <div class="time-container">
-        5:38pm
+        {{ $time }}
     </div>
 </div>
 @endif
--- resources/views/chatting/content.blade.php
+++ resources/views/chatting/content.blade.php
@@ -11,27 +11,49 @@
 </div>
 <div class="chatting__content__messages">
     @include('chatting.chat-message', [
-        'myMessage' => false
+        'myMessage' => false,
+        'content' => "Lorem  ullam vel fugiat odit aliquid asperiores cum?",
+        'time' => '5:38pm'
     ])
     @include('chatting.chat-message', [
-        'myMessage' => true
+        'myMessage' => true,
+        'content' => "Lolor sit amet consectetur adipisicing elit. Quis unde tenetur tempore expedita corrupti eos esse in nobis susc sint voluptatem quas.",
+        'time' => '9/2/20'
     ])
     @include('chatting.chat-message', [
-        'myMessage' => false
+        'myMessage' => false,
+        'content' => "Ut aliqua officia do reprehenderit cillum adipisicing esse in excepteur anim nisi exercitation aute.",
+        'time' => '9/3/20'
     ])
     @include('chatting.chat-message', [
-        'myMessage' => true
+        'myMessage' => true,
+        'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis unde tenetur minus sint voluptatem quas.",
+        'time' => '9/2/20'
     ])
     @include('chatting.chat-message', [
-        'myMessage' => false
+        'myMessage' => false,
+        'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis unde tenetur tempore expedita corrupti eos esse in nobis suscipit repudiandae ad minima magni doloribus sit, beatae minus sint voluptatem quas.",
+        'time' => '9/2/20'
     ])
     @include('chatting.chat-message', [
-        'myMessage' => false
+        'myMessage' => false,
+        'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis unde tenetur tempore expedita corrupti eos esse in nobis suscipit repudiandae ad minima magni doloribus sit, beatae minus sint voluptatem quas.",
+        'time' => '8/2/20'
     ])
     @include('chatting.chat-message', [
-        'myMessage' => true
+        'myMessage' => true,
+        'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis unde tenerepudiandae ad minima magni doloribus sit, beatae minus sint voluptatem quas.",
+        'time' => '8/1/20'
+    ]),
+    @include('chatting.chat-message', [
+        'myMessage' => true,
+        'content' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugiat laborum vero mollitia praesentium ut aliquam vel quam quos omnis neque voluptate illum magni, dolorem cum. Aut quis vero consequuntur?",
+        'time' => '9/2/20'
     ])
 </div>
-<div class="send-msg-container">
-    <h1>here</h1>
+<div class="chatting__send-msg-container">
+    <form class="search-bar-container" method="GET" action="#">
+        <input type="text" class="search-bar form-control form-control-lg" placeholder="Type here...">
+        <button class="btn btn-lg btn-primary btn-send" type="button">Send</button>
+    </form>
 </div>
