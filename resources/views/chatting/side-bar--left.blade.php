f5816ba7824c8d06c7ed07dfb809859b9f39c343
Shuaiqing Luo <shuaiqin@usc.edu>
2020-09-12 09:56:23 -0700

    update

--- resources/views/chatting/side-bar--left.blade.php
+++ resources/views/chatting/side-bar--left.blade.php
@@ -1,10 +1,56 @@
 <h4 class="heading">Message</h4>
-<div class="search-bar-container">
+<form class="search-bar-container" method="GET" action="#">
     <input type="text" class="search-bar form-control form-control-lg" placeholder="Search...">
     <svg>
         <use xlink:href="{{asset('assets/sprite.svg#icon-magnifying-glass')}}"></use>
     </svg>
-</div>
+</form>
 <ul class="chatting-msgs">
-    @include('chatting.side-bar-chatting-msg')
+    @include('chatting.side-bar-chatting-msg', [
+        'unRead' => true,
+        'time' => "5:38pm"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'unRead' => true,
+        'time' => "9/3/20"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'time' => "12/30/20"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'time' => "12/30/20"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'unRead' => true,
+        'time' => "12/30/20"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'time' => "12/30/20"
+    ])
+
+    @include('chatting.side-bar-chatting-msg', [
+        'time' => "12/30/20"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'unRead' => true,
+        'time' => "12/30/20"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'time' => "12/30/20"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'time' => "12/30/20"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'time' => "12/30/20"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'time' => "12/30/20"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'time' => "12/30/20"
+    ])
+    @include('chatting.side-bar-chatting-msg', [
+        'time' => "12/30/20"
+    ])
 </ul>
