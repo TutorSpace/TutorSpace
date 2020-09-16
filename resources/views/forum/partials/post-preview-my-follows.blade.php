1f80596940bcd3067c8077c1a8aba18f9c204a24
Shuaiqing Luo <shuaiqin@usc.edu>
2020-09-16 00:24:49 -0700

    update

--- resources/views/forum/partials/post-preview-my-follows.blade.php
+++ resources/views/forum/partials/post-preview-my-follows.blade.php
@@ -8,7 +8,7 @@
     </span>
     <p class="post__heading-2">
         <img src="{{ Storage::url($post->user->profile_pic_url) }}" alt="user photo" class="poster-img">
-        <a href="#" class="poster-name mr-2">
+        <a href="{{ route('view.profile', $post->user->id) }}" class="poster-name mr-2">
             {{ "{$post->user->first_name} {$post->user->last_name}" }}
         </a>
         <span class="mr-2 fw-900">&middot;</span>
