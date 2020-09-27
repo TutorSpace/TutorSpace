818897874f3c1335729fad7f5e60619ad4200a80
Shuaiqing Luo <shuaiqin@usc.edu>
2020-09-27 11:10:11 -0700

    update

--- public/js/home/profile.js
+++ public/js/home/profile.js
@@ -91,7 +91,7 @@
   !*** ./resources/js/home/profile.js ***!
   \**************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
 // ===================== autocomplete ==========================
 window.autocomplete = function (inp, arr, clickCallBackFunc) {
@@ -236,15 +236,13 @@ autocomplete(document.getElementById("hourly-rate"), hourlyRate);
 autocomplete(document.getElementById("course"), courses, profile_add_course);
 autocomplete(document.getElementById("tag"), tags, profile_add_tag);
 $('.boxes__course').on('click', '.box .remove', function () {
-  $(this).parent().remove(); // TODO - YASHVI: get the tag id of the element clicked
-
-  var new_tag_id = 1;
-  ajaxAddRemoveTag(new_tag_id);
+  var new_tag_id = $(this).siblings('.label').attr('data-course-id');
+  $(this).parent().remove();
+  ajaxAddRemoveCourse(new_tag_id);
 });
 $('.boxes__forum').on('click', '.box .remove', function () {
-  $(this).parent().remove(); // TODO - YASHVI: get the tag id of the element clicked
-
-  var new_tag_id = 1;
+  var new_tag_id = $(this).siblings('.label').attr('data-tag-id');
+  $(this).parent().remove();
   ajaxAddRemoveTag(new_tag_id);
 });
 
@@ -257,10 +255,10 @@ function profile_add_course() {
   else if ($('.boxes__course .box').length == 7) {
       toastr.error("You can add at most 7 courses.");
     } else {
-      appendNewBox(new_course, '.boxes__course'); // TODO - YASHVI: get the course id of the element clicked
-
+      // TODO - YASHVI: get the course id of the element clicked
+      ajaxAddRemoveCourse(new_course);
       var new_course_id = 1;
-      ajaxAddRemoveCourse(new_course_id);
+      appendNewBox('data-course-id', new_course, new_course_id, '.boxes__course');
     } // clear input field
 
 
@@ -277,66 +275,29 @@ function profile_add_tag() {
   else if ($('.boxes__forum .box').length == 10) {
       toastr.error("You can add at most 10 tags.");
     } else {
-      appendNewBox(new_tag, '.boxes__forum'); // TODO - YASHVI: get the course id of the element clicked
-
+      // TODO - YASHVI: get the course id of the element clicked
+      ajaxAddRemoveTag(new_tag);
       var new_tag_id = 1;
-      ajaxAddRemoveTag(new_tag_id);
+      appendNewBox('data-tag-id', new_tag, new_tag_id, '.boxes__forum');
     } // clear input field
 
 
   $('.profile__input__forum').val("");
-} // $('.autocomplete .profile__input__courses').on("keydown", function(e){
-//     if(e.which == 13){
-//         var new_course = $('.profile__input__courses').val().toUpperCase();
-//         // checks if a duplicate tag is being added
-//         if ($('.boxes__course .box .label').text().includes(new_course)) {
-//             toastr.error('You already added this course.');
-//         }
-//         // checks if 7 tags have been added already
-//         else if ($('.boxes__course .box').length == 7) {
-//             toastr.error("You can add at most 7 courses.");
-//         }
-//         else {
-//             appendNewBox(new_course, '.boxes__course');
-//             // TODO - YASHVI: get the tag id of the element
-//             var new_course_id = 1;
-//             ajaxAddRemoveCourse(new_course_id);
-//         }
-//         // clear input field
-//         $('.profile__input__courses').val("");
-//     }
-// });
-// $('.autocomplete .profile__input__forum').on("keydown", function(e){
-//     if(e.which == 13){
-//         var new_tag = $('.profile__input__forum').val().toUpperCase();
-//         // checks if a duplicate tag is being added
-//         if ($('.boxes__forum .box .label').text().includes(new_tag)) {
-//            // error message
-//            toastr.error('You already added this tag.');
-//         }
-//         // checks if 10 tags have been added already
-//         else if ($('.boxes__forum .box').length == 10) {
-//             toastr.error("You can add at most 10 tags.");
-//         }
-//         else {
-//             appendNewBox(new_tag, '.boxes__forum');
-//             // TODO - YASHVI: get the tag id of the element
-//             var new_tag_id = 1;
-//             ajaxAddRemoveTag(new_tag_id);
-//         }
-//         // clear input field
-//         $('.profile__input__forum').val("");
-//     }
-// });
-
+}
 
 $('#btn-reset').click(function () {
   location.reload(true);
 });
 
-function appendNewBox(tagName, parentSelector) {
-  // create new tag
-  $(parentSelector).append("\n    <span class=\"box p-relative\" id=\"new_box\" style=\"background-color: rgb(45, 118, 134); color: rgb(255, 255, 255);\">\n        <svg class=\"p-absolute verify\" width=\"1em\" height=\"1em\" viewBox=\"0 0 512 512\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n            <path d=\"M256 0C114.836 0 0 114.836 0 256C0 397.164 114.836 512 256 512C397.164 512 512 397.164 512 256C512 114.836 397.164 0 256 0Z\" fill=\"#FFCE00\"/>\n            <path d=\"M385.75 201.75L247.082 340.414C242.922 344.574 237.461 346.668 232 346.668C226.539 346.668 221.078 344.574 216.918 340.414L147.586 271.082C139.242 262.742 139.242 249.258 147.586 240.918C155.926 232.574 169.406 232.574 177.75 240.918L232 295.168L355.586 171.586C363.926 163.242 377.406 163.242 385.75 171.586C394.09 179.926 394.09 193.406 385.75 201.75V201.75Z\" fill=\"#FAFAFA\"/>\n        </svg>\n        <span class=\"label\">" + tagName + "</span>\n        <svg class=\"p-absolute remove\" width=\"1em\" height=\"1em\" viewBox=\"0 0 16 16\" fill=\"currentColor\" xmlns=\"http://www.w3.org/2000/svg\">\n            <path fill-rule=\"evenodd\" d=\"M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z\"/>\n        </svg>\n    </span>\n    ");
+function appendNewBox(dataType, tagName, tagId, parentSelector) {
+  if (dataType == 'data-course-id') {
+    // todo: make ajax call to see whether the course is verified
+    var isVerified = true;
+    var svgVerify = "<svg class=\"p-absolute verify\" width=\"1em\" height=\"1em\" viewBox=\"0 0 512 512\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n        <path d=\"M256 0C114.836 0 0 114.836 0 256C0 397.164 114.836 512 256 512C397.164 512 512 397.164 512 256C512 114.836 397.164 0 256 0Z\" fill=\"#FFCE00\"/>\n        <path d=\"M385.75 201.75L247.082 340.414C242.922 344.574 237.461 346.668 232 346.668C226.539 346.668 221.078 344.574 216.918 340.414L147.586 271.082C139.242 262.742 139.242 249.258 147.586 240.918C155.926 232.574 169.406 232.574 177.75 240.918L232 295.168L355.586 171.586C363.926 163.242 377.406 163.242 385.75 171.586C394.09 179.926 394.09 193.406 385.75 201.75V201.75Z\" fill=\"#FAFAFA\"/>\n        </svg>";
+    console.log('here');
+    $(parentSelector).append( true ? svgVerify : undefined);
+    console.log( true ? svgVerify : undefined);
+  } else if (dataType == 'data-tag-id') {}
 }
 
 function ajaxAddRemoveCourse(courseId) {
@@ -345,11 +306,11 @@ function ajaxAddRemoveCourse(courseId) {
     url: '/course_add_remove',
     data: {
       new_course_id: courseId
-    } // success: (data) => {
-    // let { successMsg } = data;
-    // toastr.success(successMsg);
-    // },
-    // error: function(error) {
+    },
+    success: function success(data) {
+      var successMsg = data.successMsg;
+      toastr.success(successMsg);
+    } // error: function(error) {
     //     toastr.error(error);
     // }
 
@@ -362,11 +323,11 @@ function ajaxAddRemoveTag(tagId) {
     url: '/tag_add_remove',
     data: {
       new_tag_id: tagId
-    } // success: (data) => {
-    // let { successMsg } = data;
-    // toastr.success(successMsg);
-    // },
-    // error: function(error) {
+    },
+    success: function success(data) {
+      var successMsg = data.successMsg;
+      toastr.success(successMsg);
+    } // error: function(error) {
     //     toastr.error(error);
     // }
 
--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -23,11 +23,6 @@ bg-student
 
 @include('partials.nav')
 
-<script>
-    @php ($course_array = ["ACAD 123415797921521512752", "ACAD 124", "ACAD 125", "ACAD 126", "ACAD 127", "ACAD 128",  "ACAD 129"])
-    @php ($forum_array = ["ACAD 123", "ACAD 124", "ACAD 125", "ACAD 126", "ACAD 127", "ACAD 128",  "ACAD 129", "ACAD 130", "ACAD 131", "ACAD 132"])
-</script>
-
 <div class="container-fluid home p-relative">
     @include('home.partials.menu_bar')
     <main class="home__content">
@@ -44,112 +39,121 @@ bg-student
 
         <form class="container col-layout-2 profile" autocomplete="off">
             <div class="row">
-                <h5 class="w-100 profile__heading">Personal Information</h5>
-                <div class="profile__form-row">
-                    <div>
-                        <label for="" class="profile__label">First Name</label>
-                        <input type="text" class="profile__input form-control form-control-lg" placeholder="Shuaiqing" readonly>
-                    </div>
-                    <div>
-                        <label for="" class="profile__label">Last Name</label>
-                        <input type="text" class="profile__input form-control form-control-lg" placeholder="Luo" disabled readonly>
-                    </div>
-                </div>
-
-                <div class="profile__form-row mt-3">
-                    <div class="autocomplete">
-                        <label for="first-major" class="profile__label">First Major</label>
-                        <input type="text" class="profile__input form-control form-control-lg" value="{{ Auth::user()->firstMajor->major ?? "" }}" name="first-major" id="first-major">
-                    </div>
-                    <div class="autocomplete">
-                        <label for="second-major" class="profile__label">Second Major (optional)</label>
-                        <input type="text" class="profile__input form-control form-control-lg" value="{{ Auth::user()->secondMajor->major ?? "" }}" name="second-major" id="second-major">
-                    </div>
-                    <div class="autocomplete">
-                        <label for="minor" class="profile__label">Minor (optional)</label>
-                        <input type="text" class="profile__input form-control form-control-lg" value="{{ Auth::user()->minor->minor ?? "" }}" name="minor" id="minor">
-                    </div>
-                </div>
-
-                <div class="profile__form-row mt-3">
-                    <div class="autocomplete">
-                        <label for="school-year" class="profile__label">Class Standing</label>
-                        <input type="text" class="profile__input form-control form-control-lg" value="{{ Auth::user()->schoolYear->school_year ?? "" }}" name="school-year" id="school-year">
-                    </div>
-                    <div class="gpa autocomplete mr-3">
-                        <label for="gpa" class="profile__label">GPA</label>
-                        <input type="text" class="profile__input form-control form-control-lg" value="{{ Auth::user()->gpa ?? "" }}" name="gpa" id="gpa">
-                    </div>
-                    <div class="gpa-note">
-                        <span class="font-italic">
-                            Note: Your GPA would <span class="font-weight-bold">NOT</span> occur on your profile page.
-                        </span>
-                    </div>
-                </div>
-
-                @if (Auth::user()->is_tutor)
-                <div class="profile__form-row mt-3">
-                    <div class="input-introduction">
-                        <label for="" class="profile__label">Introduction</label>
-                        <textarea name="" id="" cols="30" rows="10" class="profile__input form-control form-control-lg">{{ Auth::user()->getIntroduction() }}</textarea>
-                    </div>
-                </div>
-                @endif
-
-                <h5 class="w-100 profile__heading">Tutor Information</h5>
-                <div class="profile__form-row flex-wrap">
-                    <div class="autocomplete mb-3">
-                        <label for="course" class="profile__label">Courses you would like to tutor in</label>
-                        <input type="text" class="profile__input profile__input__courses form-control form-control-lg" id="course">
-                    </div>
-                    <div class="hourly-rate autocomplete">
-                        <label for="hourly-rate" class="profile__label">Hourly Rate</label>
-                        <div class="hourly-rate-input-container">
-                            <span class="symbol">$</span>
-                            <input type="text" class="profile__input form-control form-control-lg" value="{{ Auth::user()->hourly_rate ?? "" }}" name="hourly-rate" id="hourly-rate">
+                <div class="profile__text-container--white">
+                    <h5 class="w-100 font-weight-bold mb-4">Personal Information</h5>
+                    <div class="profile__form-row">
+                        <div>
+                            <label for="" class="profile__label">First Name *</label>
+                            <input type="text" class="profile__input form-control form-control-lg" placeholder="Shuaiqing" readonly>
+                        </div>
+                        <div>
+                            <label for="" class="profile__label">Last Name *</label>
+                            <input type="text" class="profile__input form-control form-control-lg" placeholder="Luo"  readonly>
                         </div>
                     </div>
 
-
-                    <div class="boxes boxes__course flex-100">
-                        @foreach ($course_array as $course)
-                        <span class="box p-relative" type="button">
-                            <svg class="p-absolute verify" width="1em" height="1em" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M256 0C114.836 0 0 114.836 0 256C0 397.164 114.836 512 256 512C397.164 512 512 397.164 512 256C512 114.836 397.164 0 256 0Z" fill="#FFCE00"/>
-                                <path d="M385.75 201.75L247.082 340.414C242.922 344.574 237.461 346.668 232 346.668C226.539 346.668 221.078 344.574 216.918 340.414L147.586 271.082C139.242 262.742 139.242 249.258 147.586 240.918C155.926 232.574 169.406 232.574 177.75 240.918L232 295.168L355.586 171.586C363.926 163.242 377.406 163.242 385.75 171.586C394.09 179.926 394.09 193.406 385.75 201.75V201.75Z" fill="#FAFAFA"/>
-                            </svg>
-                            <span class="label">{{ $course }}</span>
-                            <svg class="p-absolute remove" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                            </svg>
-                        </span>
-                        @endforeach
+                    <div class="profile__form-row mt-3">
+                        <div class="autocomplete">
+                            <label for="first-major" class="profile__label">First Major *</label>
+                            <input
+                                type="text"
+                                class="profile__input form-control form-control-lg"
+                                value="{{ Auth::user()->firstMajor->major ?? "" }}"
+                                name="first-major"
+                                id="first-major"
+                                readonly
+                            >
+                        </div>
+                        <div class="autocomplete">
+                            <label for="second-major" class="profile__label">Second Major (optional)</label>
+                            <input type="text" class="profile__input form-control form-control-lg" value="{{ Auth::user()->secondMajor->major ?? "" }}" name="second-major" id="second-major" readonly>
+                        </div>
+                        <div class="autocomplete">
+                            <label for="minor" class="profile__label">Minor (optional)</label>
+                            <input type="text" class="profile__input form-control form-control-lg" value="{{ Auth::user()->minor->minor ?? "" }}" name="minor" id="minor" readonly>
+                        </div>
                     </div>
-                    <p class="profile__label font-italic">Note: You can add at most 7 courses.</p>
+
+                    <div class="profile__form-row mt-3">
+                        <div class="autocomplete">
+                            <label for="school-year" class="profile__label">Class Standing *</label>
+                            <input type="text" class="profile__input form-control form-control-lg" value="{{ Auth::user()->schoolYear->school_year ?? "" }}" name="school-year" id="school-year" readonly>
+                        </div>
+                        <div class="gpa autocomplete mr-3">
+                            <label for="gpa" class="profile__label">GPA *</label>
+                            <input type="text" class="profile__input form-control form-control-lg" value="{{ Auth::user()->gpa ?? "" }}" name="gpa" id="gpa" readonly>
+                        </div>
+                        <div class="gpa-note">
+                            <span class="font-italic">
+                                Note: Your GPA would <span class="font-weight-bold">NOT</span> occur on your profile page.
+                            </span>
+                        </div>
+                    </div>
+
+                    @if (Auth::user()->is_tutor)
+                    <div class="profile__form-row mt-3">
+                        <div class="input-introduction">
+                            <label for="" class="profile__label">Introduction</label>
+                            <textarea name="" rows="5" class="profile__input form-control form-control-lg" readonly>{{ Auth::user()->getIntroduction() }}</textarea>
+                        </div>
+                    </div>
+                    @endif
                 </div>
 
-                <h5 class="w-100 profile__heading">Forum Settings</h5>
-                <div class="profile__form-row flex-wrap">
-                    <div class="autocomplete mb-3">
-                        <label for="tag" class="profile__label">Tags you are interested in</label>
-                        <input type="text" class="profile__input profile__input__forum form-control form-control-lg" id="tag">
-                    </div>
+                <div class="profile__text-container--white">
+                    <h5 class="w-100 font-weight-bold mb-4">Tutor Information</h5>
+                    <div class="profile__form-row flex-wrap">
+                        <div class="autocomplete mb-3">
+                            <label for="course" class="profile__label">Courses you would like to tutor in *</label>
+                            <input type="text" class="profile__input profile__input__courses form-control form-control-lg" id="course">
+                        </div>
+                        <div class="hourly-rate autocomplete">
+                            <label for="hourly-rate" class="profile__label">Hourly Rate *</label>
+                            <div class="hourly-rate-input-container">
+                                <span class="symbol">$</span>
+                                <input type="text" class="profile__input form-control form-control-lg" value="{{ Auth::user()->hourly_rate ?? "" }}" name="hourly-rate" id="hourly-rate">
+                            </div>
+                        </div>
 
-                    <div class="boxes boxes__forum flex-100">
-                    @foreach ($forum_array as $tag)
-                        <span class="box p-relative">
-                            <svg class="p-absolute verify" width="1em" height="1em" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M256 0C114.836 0 0 114.836 0 256C0 397.164 114.836 512 256 512C397.164 512 512 397.164 512 256C512 114.836 397.164 0 256 0Z" fill="#FFCE00"/>
-                                <path d="M385.75 201.75L247.082 340.414C242.922 344.574 237.461 346.668 232 346.668C226.539 346.668 221.078 344.574 216.918 340.414L147.586 271.082C139.242 262.742 139.242 249.258 147.586 240.918C155.926 232.574 169.406 232.574 177.75 240.918L232 295.168L355.586 171.586C363.926 163.242 377.406 163.242 385.75 171.586C394.09 179.926 394.09 193.406 385.75 201.75V201.75Z" fill="#FAFAFA"/>
-                            </svg>
-                            <span class="label">{{ $tag }}</span>
-                            <svg class="p-absolute remove" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                            </svg>
-                        </span>
-                        @endforeach
+
+                        <div class="boxes boxes__course flex-100">
+                            @foreach((Auth::user())->courses as $course)
+                            <span class="box p-relative" type="button">
+                                <svg class="p-absolute verify" width="1em" height="1em" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M256 0C114.836 0 0 114.836 0 256C0 397.164 114.836 512 256 512C397.164 512 512 397.164 512 256C512 114.836 397.164 0 256 0Z" fill="#FFCE00"/>
+                                    <path d="M385.75 201.75L247.082 340.414C242.922 344.574 237.461 346.668 232 346.668C226.539 346.668 221.078 344.574 216.918 340.414L147.586 271.082C139.242 262.742 139.242 249.258 147.586 240.918C155.926 232.574 169.406 232.574 177.75 240.918L232 295.168L355.586 171.586C363.926 163.242 377.406 163.242 385.75 171.586C394.09 179.926 394.09 193.406 385.75 201.75V201.75Z" fill="#FAFAFA"/>
+                                </svg>
+                                <span class="label" data-course-id={{$course->id}}>{{ $course->course }}</span>
+                                <svg class="p-absolute remove" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </span>
+                            @endforeach
+                        </div>
+                        <p class="profile__label font-italic">Note: You can add at most 7 courses.</p>
                     </div>
-                    <p class="profile__label font-italic">Note: You can add at most 10 tags.</p>
+                </div>
+
+                <div class="profile__text-container--white">
+                    <h5 class="w-100 font-weight-bold mb-4">Forum Settings</h5>
+                    <div class="profile__form-row flex-wrap">
+                        <div class="autocomplete mb-3">
+                            <label for="tag" class="profile__label">Tags you are interested in</label>
+                            <input type="text" class="profile__input profile__input__forum form-control form-control-lg" id="tag">
+                        </div>
+
+                        <div class="boxes boxes__forum flex-100">
+                        @foreach((Auth::user())->tags as $tag)
+                            <span class="box p-relative">
+                                <span class="label" data-tag-id={{$tag->id}}>{{ $tag->tag }}</span>
+                                <svg class="p-absolute remove" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </span>
+                            @endforeach
+                        </div>
+                        <p class="profile__label font-italic">Note: You can add at most 10 tags.</p>
+                    </div>
                 </div>
 
                 {{-- buttons --}}
@@ -177,43 +181,36 @@ bg-student
         "{{ $major->major }}",
         @endforeach
     ];
-
     let minors = [
         @foreach(App\Minor::all() as $minor)
         "{{ $minor->minor }}",
         @endforeach
     ];
-
     let schoolYears = [
         @foreach(App\SchoolYear::all() as $schoolYear)
         "{{ $schoolYear->school_year }}",
         @endforeach
     ];
-
     let gpa = [
         @for ($i = 4.00; $i >= 1.00; $i -= 0.01)
         "{{ number_format($i, 2) }}",
         @endfor
     ];
-
     let hourlyRate = [
         @for ($i = 10; $i <= 50; $i += 0.5)
             "{{ number_format($i, 1) }}",
         @endfor
     ];
-
     let courses = [
         @foreach(App\Course::all() as $course)
         "{{ $course->course }}",
         @endforeach
     ];
-
     let tags = [
         @foreach(App\Tag::all() as $tag)
         "{{ $tag->tag }}",
         @endforeach
     ];
-
 </script>
 
 <script src="{{ asset('js/home/profile.js') }}"></script>
