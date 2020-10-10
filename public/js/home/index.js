d94de1d9b32ec5d7cc75bbe7bfec69a52a3e9551
Shuaiqing Luo <shuaiqin@usc.edu>
2020-10-10 12:05:22 -0700

    update

--- public/js/home/index.js
+++ public/js/home/index.js
@@ -11517,6 +11517,7 @@ $("#input-profile-pic").change(function () {
     success: function success(data) {
       toastr.success('Successfully uploaded the image!');
       $('#profile-image').attr('src', storageUrl + data.imgUrl);
+      console.log(storageUrl + data.imgUrl);
       $('.nav-right__profile-img').attr('src', storageUrl + data.imgUrl);
     },
     error: function error(_error) {
