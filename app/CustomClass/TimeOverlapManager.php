7c8dfcb46f31f78071a4b23788b4a142283dc9be
anuragunni <anurag.unnikrishnan@gmail.com>
2020-10-13 23:44:41 -0700

    Changes to the available time formula to search for a tutor

---
+++ app/CustomClass/TimeOverlapManager.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\CustomClass;
+
+class TimeOverlapManager {
+
+  //take the user input as $startTime and $endTime,
+  //compares it with the tutor's time - 
+  //$availableTimeStart and $availableTimeEnd and RETURNS TRUE IF THERE IS NO OVERLAP
+  public static function timeOverlap($startTime, $endTime, $availableTimeStart, $availableTimeEnd) {
+    
+    return ($endTime <= $availableTimeStart) || ($availableTimeEnd <= $startTime);
+  }
+}
