80aa7bbb37ba144aa16af3f282daffa7d577d402
Shuaiqing Luo <shuaiqin@usc.edu>
2020-10-15 00:46:57 -0700

    udpadte

--- app/CustomClass/TimeOverlapManager.php
+++ app/CustomClass/TimeOverlapManager.php
@@ -4,10 +4,9 @@ namespace App\CustomClass;
 class TimeOverlapManager {
 
   //take the user input as $startTime and $endTime,
-  //compares it with the tutor's time - 
+  //compares it with the tutor's time -
   //$availableTimeStart and $availableTimeEnd and RETURNS TRUE IF THERE IS NO OVERLAP
-  public static function noTimeOverlap($startTime, $endTime, $availableTimeStart, $availableTimeEnd) {
-    
-    return ($endTime <= $availableTimeStart) || ($availableTimeEnd <= $startTime);
-  }
+    public static function noTimeOverlap($startTime, $endTime, $availableTimeStart, $availableTimeEnd) {
+        return ($endTime <= $availableTimeStart) || ($availableTimeEnd <= $startTime);
+    }
 }
