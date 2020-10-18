20bfbf542b0aeabc29c6cb77c590b3265b231645
Shuaiqing Luo <shuaiqin@usc.edu>
2020-10-17 18:21:48 -0700

    update

--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -41,15 +41,18 @@ bg-student
             @method('PUT')
             @csrf
             <div class="row">
-                <div class="profile__text-container--white profile__tutor-info">
+                <div class="profile__text-container--white profile__info @if (isset($registerToBeTutor) && $registerToBeTutor) z-index-9999 @endif">
                     @if ($errors->any())
                     <p class="fs-1-4 fc-red mb-2">
                         {{ $errors->first() }}
                     </p>
                     @endif
-                    <div class="d-flex justify-content-between align-items-center mb-4">
+                    <div class="d-flex justify-content-between align-items-center mb-4 p-relative">
+                        @if (isset($registerToBeTutor) && $registerToBeTutor)
+                        <h4 class="heading--register-to-be-tutor">Step 1: Complete your personal infomation</h4>
+                        @endif
                         <h5 class="font-weight-bold">Personal Information</h5>
-                        <div class="profile__text__edit d-flex align-items-center mr-2 hover--pointer">
+                        <div class="profile__text__edit d-flex align-items-center mr-2 hover--pointer @if (isset($registerToBeTutor) && $registerToBeTutor) hidden @endif">
                             <svg class="mr-2" width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"/>
                                 <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"/>
@@ -117,8 +120,12 @@ bg-student
 
                     {{-- buttons --}}
                     <div class="w-100 profile__buttons d-none">
+                        @if (isset($registerToBeTutor) && $registerToBeTutor)
+                        <button class="btn btn-primary" type="submit">Next</button>
+                        @else
                         <button class="btn btn-outline-primary mr-5" id="btn-reset" type="button">Discard Changes</button>
                         <button class="btn btn-primary" type="submit">Save Changes</button>
+                        @endif
                     </div>
                 </div>
 
@@ -276,4 +283,12 @@ bg-student
 
 <script src="{{ asset('js/home/profile.js') }}"></script>
 
+@if (isset($registerToBeTutor) && $registerToBeTutor)
+<script>
+    $('.profile__text__edit').click();
+</script>
+
+{{-- the following classes are from the bootbox --}}
+<div class="modal-backdrop fade show"></div>
+@endif
 @endsection
