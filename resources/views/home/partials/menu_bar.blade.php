605329ebf409a972ac053c674db256240c53f75e
Shuaiqing Luo <shuaiqin@usc.edu>
2020-10-24 22:33:46 -0700

    update

--- resources/views/home/partials/menu_bar.blade.php
+++ resources/views/home/partials/menu_bar.blade.php
@@ -1,3 +1,50 @@
+<div class="toggle-customized color-primary">
+    <div class="toggle-container toggle-collapsed">
+        <svg class="svg-toggle" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-right-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+        </svg>
+    </div>
+    <div class="toggle-container toggle-expanded">
+        <svg class="svg-toggle" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-right-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+        </svg>
+    </div>
+    <ul class="toggle-after-list">
+        <li class="toggle-after-list-item">
+            <a class="toggle-after-list-content" href="#">
+                <svg class="toggle-after-list-svg">
+                    <use xlink:href="{{asset('assets/sprite.svg#icon-home')}}"></use>
+                </svg>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="toggle-after-list-item">
+            <a class="toggle-after-list-content" href="#">
+                <svg class="toggle-after-list-svg">
+                    <use xlink:href="{{asset('assets/sprite.svg#icon-home')}}"></use>
+                </svg>
+                <span>Tutor Sessions</span>
+            </a>
+        </li>
+        <li class="toggle-after-list-item">
+            <a class="toggle-after-list-content" href="#">
+                <svg class="toggle-after-list-svg">
+                    <use xlink:href="{{asset('assets/sprite.svg#icon-home')}}"></use>
+                </svg>
+                <span>Forum Activities</span>
+            </a>
+        </li>
+        <li class="toggle-after-list-item">
+            <a class="toggle-after-list-content" href="#">
+                <svg class="toggle-after-list-svg">
+                    <use xlink:href="{{asset('assets/sprite.svg#icon-home')}}"></use>
+                </svg>
+                <span>Profile Settings</span>
+            </a>
+        </li>
+    </ul>
+</div>
+
 <nav class="menu-bar">
     <ul class="menu-bar__lists">
         <li @if(Route::current()->getName() == 'home') class="active" @endif>
