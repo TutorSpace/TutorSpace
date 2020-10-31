b4bbf88b012377913d183d2d8b8fafb07f869a01
Shuaiqing Luo <shuaiqin@usc.edu>
2020-10-31 15:57:43 -0700

    initial merge with help center

---
+++ resources/views/help-center/partials/support-card.blade.php
@@ -0,0 +1,8 @@
+
+<div class="d-flex flex-column justify-content-center align-items-center bg-white px-5 pb-5 support__card mx-3 my-4">
+    <svg class="mb-3" width="5rem" height="5rem" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="{{ $SVG }}" fill="#6749DF"/>
+    </svg>
+    <p class="fs-2 font-weight-bold support__card__title mb-3">{{ $Title }}</p>
+    <p class="fc-grey fs-1-4 text-center">{{ $Content}}</p>
+</div>
