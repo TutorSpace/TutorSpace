c30856fef8540fd45a07f791f4c80b07725a8d96
Shuaiqing Luo <shuaiqin@usc.edu>
2020-11-01 16:28:19 -0800

    update styles

--- resources/js/forget_password.js
+++
@@ -1,9 +0,0 @@
-$('#btn-back').click(() => {
-    window.location.href = '/login';
-});
-
-
-$('#btn-send').click(() => {
-    alert('Forget password functionality is not completed yet.')
-    // window.location.href = '/forget_password_verify';
-});
