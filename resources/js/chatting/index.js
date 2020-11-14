295f3e3f1fb414eaecc7a2c686c03c20816e590d
Shuaiqing Luo <shuaiqin@usc.edu>
2020-11-14 11:33:04 -0800

    update

--- resources/js/chatting/index.js
+++ resources/js/chatting/index.js
@@ -2,6 +2,37 @@ function scrollToBottom() {
     $('.chatting__content__messages').scrollTop($('.chatting__content__messages')[0].scrollHeight);
 }
 
+function appendSendMsgFunc() {
+    // sending messages
+    $('#msg-form').submit(function() {
+        if($('#msg-to-send').val()) {
+            $.ajax({
+                type:'POST',
+                url: '/chatting/send-msg',
+                data: $('#msg-form').serialize(),
+                success: (data) => {
+                    console.log(data);
+                }
+            });
+
+            let el = `<div class="message message--self">
+                <div class="time-container">
+                    Now
+                </div>
+                <div class="message-content-container">
+                    ${$('#msg-to-send').val()}
+                </div>
+            </div>`;
+
+            $('.chatting__content__messages').append(el);
+            scrollToBottom();
+
+            $('#msg-to-send').val('');
+        }
+        return false;
+    });
+}
+
 $('.msg').click(function() {
     $('.msg .box').removeClass('bg-grey-light');
     $(this).find('.box').addClass('bg-grey-light');
@@ -16,36 +47,9 @@ $('.msg').click(function() {
             $(this).removeClass('unread');
             $('.chatting__content').html(data);
             scrollToBottom();
+            appendSendMsgFunc();
         }
     });
 });
 
 
-// sending messages
-$('#msg-form').submit(function() {
-    if($('#msg-to-send').val()) {
-        $.ajax({
-            type:'POST',
-            url: '/chatting/send-msg',
-            data: $('#msg-form').serialize(),
-            success: (data) => {
-                console.log(data);
-            }
-        });
-
-        let el = `<div class="message message--self">
-            <div class="time-container">
-                Now
-            </div>
-            <div class="message-content-container">
-                ${$('#msg-to-send').val()}
-            </div>
-        </div>`;
-
-        $('.chatting__content__messages').append(el);
-        scrollToBottom();
-
-        $('#msg-to-send').val('');
-    }
-    return false;
-});
