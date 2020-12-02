393b09abf8f259d777508955ead708a9b3a683c3
Shuaiqing Luo <shuaiqin@usc.edu>
2020-12-02 13:01:14 -0800

    update

--- resources/js/home_student.js
+++
@@ -1,9 +0,0 @@
-$('.recommended__tutors tr >:not(:last-child)').click(function() {
-    window.location.href = '/view_profile/' + $(this).parent().attr('data-user-id') + '?from=home';
-});
-
-
-$('.tutor-container .btn-view-profile').click(function() {
-    window.location.href = '/view_profile/' + $(this).attr('data-user-id') + '?from=home';
-})
-
