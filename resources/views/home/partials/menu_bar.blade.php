c94dbfa4627b3f4761d80d492f3e37c7c0b4ebf0
Shuaiqing Luo <shuaiqin@usc.edu>
2020-12-05 00:28:52 -0800

    update

--- resources/views/home/partials/menu_bar.blade.php
+++ resources/views/home/partials/menu_bar.blade.php
@@ -50,7 +50,7 @@
         <li @if(Route::current()->getName() == 'home') class="active" @endif>
             <a href="{{ route('home') }}">
                 <span>Dashboard</span>
-                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right arrow" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                 </svg>
             </a>
@@ -59,7 +59,7 @@
         <li @if(Route::current()->getName() == 'home.tutor-sessions') class="active" @endif>
             <a href="{{ route('home.tutor-sessions') }}">
                 <span>Tutor Sessions</span>
-                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right arrow" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                 </svg>
             </a>
@@ -67,14 +67,13 @@
         <li @if(Route::current()->getName() == 'home.forum-activities') class="active" @endif>
             <a href="{{ route('home.forum-activities') }}">
                 <span>Forum Activities</span>
-                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right arrow" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                 </svg>
             </a>
         </li>
         <li @if(Route::current()->getName() == 'home.profile') class="active" @endif>
             <a href="{{ route('home.profile') }}">
-
                 <div class="menu-bar-notification">
                     <span>Profile Settings</span>
                     @if(Auth::user()->is_tutor && Auth::user()->tutor_verification_status == "unsubmitted")
@@ -89,8 +88,6 @@
                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right arrow" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                 </svg>
-
-
 
             </a>
         </li>
