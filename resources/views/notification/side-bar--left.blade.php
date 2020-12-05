d91c5a7f33b7423395b1e978b9fe888de64bdc0a
Shuaiqing Luo <shuaiqin@usc.edu>
2020-12-05 10:53:16 -0800

    update

--- resources/views/notification/side-bar--left.blade.php
+++ resources/views/notification/side-bar--left.blade.php
@@ -1,10 +1,10 @@
 <h4 class="heading">Notification</h4>
-<form class="search-bar-container" method="GET" action="#">
+{{-- <form class="search-bar-container" method="GET" action="#">
     <input type="text" class="search-bar form-control form-control-lg" placeholder="Search...">
     <svg>
         <use xlink:href="{{asset('assets/sprite.svg#icon-magnifying-glass')}}"></use>
     </svg>
-</form>
+</form> --}}
 <div class="notification-type-info">
     <div class="type-container">
         <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
