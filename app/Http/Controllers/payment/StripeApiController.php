b4d4f8f560bc64eb88571b3d602aa2726f10b64f
Lihan Zhu <lihanzhu@usc.edu>
2020-12-19 20:32:02 -0800

    Update style

--- app/Http/Controllers/payment/StripeApiController.php
+++ app/Http/Controllers/payment/StripeApiController.php
@@ -445,15 +445,18 @@ class StripeApiController extends Controller
                 Log::debug('invoice.paid received' . $invoice->id);
 
                 // Change database transaction invoice_status => paid
-                $transaction = Transaction::where("invoice_id",$invoice->id)->get()[0];
+                $transaction = Transaction::where("invoice_id", $invoice->id)->get()[0];
                 $transaction->invoice_status = 'paid';
                 $transaction->save();
                 break;
+
             case 'charge.refund.updated':
                 $refund = $event->data->object;
-                // TODO: check refund using email?
                 Log::debug('charge.refund.updated received' . $refund->id);
+                // TODO: check refund using email?
+                
                 break;
+                
             // Handle other event types
             default:
                 Log::debug('Received unknown event type ' . $event->type);
