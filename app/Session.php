aa6be8df2ba7630c0bb82e316a3e4510d4b8fb33
Lihan Zhu <lihanzhu@usc.edu>
2021-01-06 14:35:28 -0800

    Finish exp

--- app/Session.php
+++ app/Session.php
@@ -43,6 +43,13 @@ class Session extends Model
         }
     }
 
+    // Calculate the duration in hour (with two decimal places)
+    public function getDurationInHour() {
+        $startTimeInTime = strtotime($this->session_time_start);
+        $endTimeInTime = strtotime($this->session_time_end);
+        return round(abs($endTimeInTime - $startTimeInTime) / 3600, 2);;
+    }
+
     public static function hasOverlapSession($startTime, $endTime, $user){
 
     }
