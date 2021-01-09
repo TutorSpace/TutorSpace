6fb87660ddbc458478718b2323d28b8e0aeeb784
Shuaiqing Luo <shuaiqin@usc.edu>
2021-01-08 17:50:21 -0800

    update

--- resources/views/home/view_profile/partials/forum.blade.php
+++ resources/views/home/view_profile/partials/forum.blade.php
@@ -7,5 +7,21 @@
            <option value="">Sort by Time (Descending)</option>
        </select>
     </div>
-
+    <div class="post-previews">
+        @include('forum.partials.post-preview-general', [
+            'posts' => App\Post::with([
+                            'tags',
+                            'user'
+                        ])
+                        ->withCount([
+                            'usersUpvoted',
+                            'replies',
+                            'tags'
+                        ])
+                        // todo: modify the formula
+                        ->where('user_id', $user->id)
+                        ->orderByRaw(App\POST::POPULARITY_FORMULA)
+                        ->get()
+        ])
+    </div>
 </section>
