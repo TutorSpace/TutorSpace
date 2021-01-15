36623302aba4953f5fd80ec31c79134d4f283f1f
Shuaiqing Luo <shuaiqin@usc.edu>
2021-01-15 12:20:32 -0800

    update

--- database/seeds/ReplySeeder.php
+++ database/seeds/ReplySeeder.php
@@ -13,39 +13,39 @@ class ReplySeeder extends Seeder
     public function run()
     {
         // Reply::create([
-        //     'post_id' => 1,
-        //     'user_id' => 2,
+        //     'post_id' => $postIds[0],
+        //     'user_id' => $userIds[1],
         //     'is_direct_reply' => true,
         //     'reply_content' => 'This is the first reply',
-        //     'created_at' => '2020-07-12 23:18:16'
+        //     'created_at' => '2021-01-12 23:18:16'
         // ]);
 
         // Reply::create([
-        //     'post_id' => 1,
+        //     'post_id' => $postIds[0],
         //     'reply_id' => 1,
         //     'base_reply_id' => 1,
-        //     'user_id' => 1,
+        //     'user_id' => $userIds[0],
         //     'is_direct_reply' => false,
         //     'reply_content' => 'This is the second reply',
-        //     'created_at' => '2020-07-12 23:18:24'
+        //     'created_at' => '2021-01-12 23:18:24'
         // ]);
 
         // Reply::create([
-        //     'post_id' => 1,
+        //     'post_id' => $postIds[0],
         //     'reply_id' => 2,
         //     'base_reply_id' => 1,
-        //     'user_id' => 2,
+        //     'user_id' => $userIds[1],
         //     'is_direct_reply' => false,
         //     'reply_content' => 'This is the third reply',
-        //     'created_at' => '2020-07-12 23:23:24'
+        //     'created_at' => '2021-01-12 23:23:24'
         // ]);
 
         // Reply::create([
-        //     'post_id' => 1,
-        //     'user_id' => 1,
+        //     'post_id' => $postIds[0],
+        //     'user_id' => $userIds[0],
         //     'is_direct_reply' => true,
         //     'reply_content' => 'This is the fourth reply',
-        //     'created_at' => '2020-07-12 23:24:16'
+        //     'created_at' => '2021-01-12 23:24:16'
         // ]);
 
 
