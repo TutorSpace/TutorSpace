785a5a4c9bcd5692d1bcc748aaf3166fa962583f
Shuaiqing Luo <shuaiqin@usc.edu>
2021-01-15 14:34:36 -0800

    update

--- database/seeds/ReviewSeeder.php
+++ database/seeds/ReviewSeeder.php
@@ -11,15 +11,15 @@ class ReviewSeeder extends Seeder
      */
     public function run()
     {
-        $userIds = DatabaseSeeder::$userIds;
-        $pastSessionIds = DatabaseSeeder::$pastSessionIds;
+        // $userIds = DatabaseSeeder::$userIds;
+        // $pastSessionIds = DatabaseSeeder::$pastSessionIds;
 
-        DB::table('reviews')->insert([
-            'star_rating' => 4,
-            'reviewer_id' => $userIds[0],
-            'reviewee_id' => $userIds[2],
-            'review' => 'This is testing review 1.',
-            'session_id' => $pastSessionIds[0],
-        ]);
+        // DB::table('reviews')->insert([
+        //     'star_rating' => 4,
+        //     'reviewer_id' => $userIds[0],
+        //     'reviewee_id' => $userIds[2],
+        //     'review' => 'This is testing review 1.',
+        //     'session_id' => $pastSessionIds[0],
+        // ]);
     }
 }
