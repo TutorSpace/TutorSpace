8ba659576b20852d92d28d3e11455270db340d32
Shuaiqing Luo <shuaiqin@usc.edu>
2021-01-18 21:55:59 -0800

    update

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
+@extends('layouts.app')
 
-<head>
-    <title>Pusher Test</title>
-    <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+@section('content')
 
-</head>
-
-<body>
-    <h1> Test</h1>
-    <h4> Test</h4>
-    <span> Test </span>
-    <!-- <p>
-    Try publishing an event to channel <code>my-channel</code>
-    with event name <code>my-event</code>.
-  </p> -->
-</body>
+<div class="container mt-5">
+    <h1 class="text-center"> Test</h1>
+    <h4 class="text-center"> Test</h4>
+    <p class="text-center fs-2-2">Test</p>
+</div>
+@endsection
