d92528fd31f147fa128547fdef60573f3cf72e7b
Shuaiqing Luo <shuaiqin@usc.edu>
2021-01-19 16:42:12 -0800

    update

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -66,7 +66,6 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                // 'shuaiqin@usc.edu'
             ]);
         });
     }
