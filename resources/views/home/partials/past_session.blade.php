64e7eabf475618c864b4c301da5b81511aa04d78
Shuaiqing Luo <shuaiqin@usc.edu>
2021-01-23 16:29:41 -0800

    update

--- resources/views/home/partials/past_session.blade.php
+++ resources/views/home/partials/past_session.blade.php
@@ -14,7 +14,7 @@ $tz = App\CustomClass\TimeFormatter::getTZ();
             <span class="title show--sm">Date</span>
             <div>
                 <span class="content">
-                    {{ date("m/d", strtotime($session->session_time_start))->setTimezone(new DateTimeZone($tz) }}<span class="info-box__year">{{ date("/y", strtotime($session->session_time_start)) }}</span>
+                    {{ date("m/d", strtotime($session->session_time_start))->setTimezone(new DateTimeZone($tz)) }}<span class="info-box__year">{{ date("/y", strtotime($session->session_time_start)) }}</span>
                     {{ date("D", strtotime($session->session_time_start)) }}
                 </span>
             </div>
