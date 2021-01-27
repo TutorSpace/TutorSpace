6468e464a8ecea354933fdb73e519be2f98f3048
Shuaiqing Luo <shuaiqin@usc.edu>
2021-01-26 22:26:39 -0800

    update

--- resources/views/notification/side-bar--left.blade.php
+++ resources/views/notification/side-bar--left.blade.php
@@ -10,19 +10,19 @@
         <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="7.5" cy="7.5" r="7.5" fill="#F0847D"/>
         </svg>
-        Sessions
+        <a href="#" class="fc-black-2">Sessions</a>
     </div>
     <div class="type-container">
         <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="7.5" cy="7.5" r="7.5" fill="#FBB752"/>
         </svg>
-        Forum
+        <a href="#" class="fc-black-2">Forum</a>
     </div>
     <div class="type-container">
         <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="7.5" cy="7.5" r="7.5" fill="@if(Auth::user()->is_tutor) #6749DF @else #1F7AFF @endif"/>
         </svg>
-        TutorSpace
+        <a href="#" class="fc-black-2">TutorSpace</a>
     </div>
 </div>
 <ul class="msgs">
