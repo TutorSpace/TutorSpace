0ecb6b58f2f86f7b6a40c91186cb93b34ae679fc
Shuaiqing Luo <shuaiqin@usc.edu>
2021-01-31 02:34:34 -0800

    update

--- resources/js/forum/create.js
+++ resources/js/forum/create.js
@@ -1,3 +1,5 @@
+import tippy, {animateFill} from 'tippy.js';
+
 $('.btn-post-type').click(function() {
     $('.btn-selected').removeClass('btn-selected');
     $(this).addClass('btn-selected');
@@ -15,4 +17,37 @@ $('#create-tags').select2({
             };
         }
     }
+});
+
+tippy($('#btn-question')[0], {
+    animateFill: false,
+    plugins: [animateFill],
+    content: 'Any questions you have regarding course content and professors, school majors, puzzles in your homework, and anything else you may find confused in your school life.',
+    interactive: true,
+    placement: 'top',
+    // interactiveDebounce: 75,
+    allowHTML: true,
+    theme: 'help-content',
+});
+
+tippy($('#btn-class-note')[0], {
+    animateFill: false,
+    plugins: [animateFill],
+    content: 'If you believe your class notes will help students who are currently taking or will take a particular course in the future, you are welcomed to share them and let more people benefit. However, please note that any sharing should comply with the USC Integrity Policy.',
+    interactive: true,
+    placement: 'top',
+    // interactiveDebounce: 75,
+    allowHTML: true,
+    theme: 'help-content',
+});
+
+tippy($('#btn-class-review')[0], {
+    animateFill: false,
+    plugins: [animateFill],
+    content: 'You can share your thoughts on what is the primary content of the course, how do you like your professor compared to professors in other sections, how do you think this course may contribute to the completion of your degree and your career aspiration, what kinds of students do you think should take this course, any tips for students to ace this course, etc.',
+    interactive: true,
+    placement: 'top',
+    // interactiveDebounce: 75,
+    allowHTML: true,
+    theme: 'help-content',
 });
