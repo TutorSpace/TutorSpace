44afff15c1cc61424dd920adcd394161acde6c1f
Shuaiqing Luo <shuaiqin@usc.edu>
2021-01-31 02:25:20 -0800

    update

--- resources/views/notification/side-bar--left.blade.php
+++ resources/views/notification/side-bar--left.blade.php
@@ -8,6 +8,12 @@
 <div class="notification-type-info">
     <div class="type-container">
         <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="7.5" cy="7.5" r="7.5" fill="#A5BE0D"/>
+        </svg>
+        <a href="{{ route('notifications.index') }}" class="fc-black-2">All</a>
+    </div>
+    <div class="type-container">
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="7.5" cy="7.5" r="7.5" fill="#F0847D"/>
         </svg>
         <a href="{{ route('notifications.index') . '?showSessions=true' }}" class="fc-black-2">Sessions</a>
