514da6b78f8fa5328b95cc8ec52be337605271cd
Shuaiqing Luo <shuaiqin@usc.edu>
2021-01-30 22:02:14 -0800

    update

--- resources/views/home/partials/bookmarked-tutor.blade.php
+++ resources/views/home/partials/bookmarked-tutor.blade.php
@@ -1,5 +1,12 @@
 <div class="bookmarked-user @if(isset($hidden) && $hidden) hidden @endif" @if(isset($hidden) && $hidden) data-to-hide="true" @endif" data-user-id="{{ $user->id }}">
-    <img src="{{ Storage::url($user->profile_pic_url) }}" alt="bookmarked user">
+    @if (Illuminate\Support\Str::of($user->profile_pic_url)->contains('placeholder'))
+    <div class="user-img placeholder-img">
+        <span>{{ strtoupper($user->first_name[0]) . ' ' . strtoupper($user->last_name[0]) }}</span>
+    </div>
+    @else
+    <img src="{{ Storage::url($user->profile_pic_url) }}" alt="bookmarked user" class="user-img">
+    @endif
+
     <div class="bookmarked-user__content">
         <a class="user-name fc-black-2" href="{{ route('view.profile', $user) }}">{{ $user->first_name }} {{ $user->last_name }}</a>
         <div class="buttons">
