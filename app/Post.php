3d6ee84f0d8f07a47b2e853d95c68d50f60dae86
Shuaiqing Luo <shuaiqin@usc.edu>
2021-02-02 08:29:09 -0800

    update

--- app/Post.php
+++ app/Post.php
@@ -222,11 +222,7 @@ class Post extends Model
                     ->take(15)
                     ->get();
 
-        echo "posts";
-        dd($this->queryYouMayHelpWith()->join('post_tag', 'posts.id', '=', 'post_tag.post_id')->join('tags', 'tags.id', '=', 'post_tag.tag_id')->whereIn('tags.id', $interestedTagIDs)
-        ->where('posts.user_id', '!=', $user->id)
-        ->groupBy(['posts.id'])
-        ->take(15)->get());
+
 
         if($posts->count() < 5) {
             $posts = $posts->merge(
